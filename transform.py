1dbbf1fce5e3b19a1826b55b075e33dd01cb8b64
sunlumos <735287480@qq.com>
2023-05-05 19:47:52 +0800

    引入tb显示

--- transform.py
+++ transform.py
@@ -1,7 +1,9 @@
 from PIL import Image
 from torchvision import transforms
+from torch.utils.tensorboard import SummaryWriter
 
-import cv2 
+
+# import cv2 
 
 
 # python的用法 =》 tensor数据类型  两个问题：
@@ -11,7 +13,9 @@ import cv2
 img_path = "./hymenoptera_data/train/ants/0013035.jpg"
 img = Image.open(img_path)
 # opencv读取图像
-cv_img = cv2.imread(img_path)
+# cv_img = cv2.imread(img_path)
+
+writer = SummaryWriter("logs")
 
 #! 1.如何使用transforms
 # 将图像转化为tensor类型
@@ -19,4 +23,8 @@ tensor_trans = transforms.ToTensor()
 # tensor_trans(img)实际上是调用了transforms.ToTensor().__call__(img)，其中img是输入参数。
 tensor_img = tensor_trans(img) 
 
-print(tensor_img)
+# print(tensor_img)
+
+writer.add_image("Tensor_img", tensor_img)
+
+writer.close() 
