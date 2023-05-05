e206ffcd000985df24fb42eeeaf0302a0a90a16b
sunlumos <735287480@qq.com>
2023-05-05 19:05:54 +0800

    opencv打开图片

--- transform.py
+++ transform.py
@@ -1,13 +1,16 @@
 from PIL import Image
 from torchvision import transforms
 
+import cv2 
+
 # python的用法 =》 tensor数据类型  两个问题：
 # 1.如何使用transforms
 # 2.为什么我们需要Tensor数据类型
 
 img_path = "./hymenoptera_data/train/ants/0013035.jpg"
 img = Image.open(img_path)
-
+# opencv读取图像
+cv_img = cv2.imread(img_path)
 
 #! 1.如何使用transforms
 # 将图像转化为tensor类型
