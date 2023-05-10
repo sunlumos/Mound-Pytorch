a70e99d997c16bd853b3a92f07be18a515f0a372
sunlumos <735287480@qq.com>
2023-05-10 20:28:37 +0800

    提取数据并进行展示

--- datasets_transforms.py
+++ datasets_transforms.py
@@ -4,3 +4,12 @@ train_set = torchvision.datasets.CIFAR10(root="./datasets", train=True, download
 test_set = torchvision.datasets.CIFAR10(root="./datasets", train=False, download=False)
 
 print(test_set[0])
+print(test_set.classes)
+
+img, target = test_set[0]
+print(img)
+print(target)
+# 展示类别
+print(test_set.classes[target])
+
+img.show()
