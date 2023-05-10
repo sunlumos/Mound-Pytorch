87049e1e241c965242e06ceecb3e73bc87b10796
sunlumos <735287480@qq.com>
2023-05-10 21:07:19 +0800

    讲解更新

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Mound-Pytorch
 B站小土堆pytorch学习
+
+## dataset
+Dataset（数据集）是指训练、验证或测试模型所使用的数据的集合。Dataset通常由数据样本组成，每个样本包含一个或多个特征和相应的标签或目标值。
+## dataloader
+负责取出数据
--- datasets_transforms.py
+++ datasets_transforms.py
@@ -1,15 +1,26 @@
 import torchvision
-
-train_set = torchvision.datasets.CIFAR10(root="./datasets", train=True, download=False)
-test_set = torchvision.datasets.CIFAR10(root="./datasets", train=False, download=False)
+from torch.utils.tensorboard import SummaryWriter
+dataset_transform = torchvision.transforms.Compose([
+torchvision.transforms .ToTensor()
+])
+train_set = torchvision.datasets.CIFAR10(root="./datasets", train=True, transform= dataset_transform, download=False)
+test_set = torchvision.datasets.CIFAR10(root="./datasets", train=False, transform= dataset_transform, download=False)
 
 print(test_set[0])
-print(test_set.classes)
 
-img, target = test_set[0]
-print(img)
-print(target)
-# 展示类别
-print(test_set.classes[target])
+writer = SummaryWriter("p10")
+for i in range(10):
+    img, target = test_set[i]
+    writer.add_image("test_set", img, i)
+    
+writer.close()
 
-img.show()
+# print(test_set.classes)
+
+# img, target = test_set[0]
+# print(img)
+# print(target)
+# # 展示类别
+# print(test_set.classes[target])
+
+# img.show()
